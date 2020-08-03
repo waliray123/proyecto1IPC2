644da4853443fcb3ea54d11d6ba5dc99506ef95b
William <willy2000.wld@gmail.com>
2020-08-03 09:48:51 -0600

    Primer commit

---
+++ src/main/java/GUIStart/Main.java
@@ -0,0 +1,15 @@
+
+package GUIStart;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StartFrame startFrame = new StartFrame();
+        startFrame.setVisible(true);
+    }
+    
+}
